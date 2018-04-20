4f89f742a9ca14f4a31ac57d87c1f42874ea78cc
Jeff Vega <jeff@intellibliss.com>
2018-04-20 14:06:22 -0700

    getting cheesy up in here

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Chesselist from './components/cheese-list'
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import store from './store'
+
+
+
+ReactDOM.render(
+<Provider store={store}>
+<Chesselist/>
+</Provider>
+, document.getElementById('root'));
+registerServiceWorker();
